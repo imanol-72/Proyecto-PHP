1187fcf57eb19c8ff55b642ef76872746fdd6df3
IAWE <IAWE@3masanz-A13-04>
2023-02-02 13:13:53 +0100

    Cabecera del proyecto

---
+++ cabecera.php
@@ -0,0 +1,18 @@
+﻿<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <title>Aridaners - Proyecto PHP</title>
+        <link rel="stylesheet" type="text/css" href="css/estilo.css" />
+    </head>
+
+
+    <!-- el cuerpo  -->
+    <body>
+    <div id="contenido">
+        <div id="cabecera">
+            <h1>probando web del proyecto</h1>
+        </div> <!-- fin de cabecera -->
+        <div id="principal">
+            <?php include $menu?>  
+            <div id="derecha">
