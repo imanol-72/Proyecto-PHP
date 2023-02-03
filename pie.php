abe95427be64ab5e101c36e46fb4583db0d1ab67
imanol-72 <124252106+imanol-72@users.noreply.github.com>
2023-02-03 08:42:03 +0100

    Add files via upload

---
+++ pie.php
@@ -0,0 +1,10 @@
+</div>
+</div>
+    <div id="pie">
+            <p class="copyright">
+                &copy; <?php echo date("Y"); ?> Proyecto PHP - Aridaners
+            </p>
+     </div>
+    </div>
+    </body>
+</html>
