fd773bc2d1938711cab63461eba8e2fb2f86984f
imanol-72 <124252106+imanol-72@users.noreply.github.com>
2023-02-03 08:43:32 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-              // put your code here
-        ?>
-    </body>
-</html>
+<?php
+      
+    $menu = "menu.php";
+    include "cabecera.php";
+
+?>
+
+<p>pagina de index de prueba</p>
+
+<?php
+    
+    include "pie.php";
+    
+?>
