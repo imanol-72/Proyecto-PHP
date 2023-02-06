ba8ce76dcf55326984d7b8e316010e352429a023
patupau <124569648+patupau@users.noreply.github.com>
2023-02-06 10:43:32 +0100

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,7 @@
+var modalBtn = document.querySelector('.modal-btn');
+var modalContent = document.querySelector('.modal-content');
+
+
+modalBtn.addEventListener('click',function(){
+    modalContent.classList.add('content-active')
+});
