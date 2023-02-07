431854d00f5ed5e763f6b8050c7a02efa8f41797
imanol-72 <124252106+imanol-72@users.noreply.github.com>
2023-02-07 10:55:43 +0100

    cambio del pie de pagina

--- pie.php
+++ pie.php
@@ -1,10 +1,39 @@
-</div>
-</div>
-    <div id="pie">
-            <p class="copyright">
-                &copy; <?php echo date("Y"); ?> Proyecto PHP - Aridaners
-            </p>
-     </div>
+<div class="footer__container">
+
+<div class="footer__links">
+
+    <div class="footer__link--wrapper">
+
+        <div class="footer__link--items">
+
+            <h2>About Us</h2>
+
+            <a href="/sign__up">How do we work</a> 
+
+            <a href="/">Terms of Service</a>
+                        
+        </div>
+
+
+        <div class="footer__link--items">
+
+            <h2>Contact Us</h2>
+
+            <a href="/">Contact</a> 
+            
+            <a href="/">Support</a>
+
+        </div>
     </div>
-    </body>
+</div>
+
+
+    <div class="social__media--wrap">
+        <p class="website__rights">© STC 2021. All rights reserved</p>
+    </div>
+</div>          
+<div id="progressbar"></div>
+<div id="scrollPath"></div>
+<script src="/js/scrollbar.js"></script>
+</body>
 </html>
