3a13ab047555612c219e0e3e84b261f3a7188385
patupau <124569648+patupau@users.noreply.github.com>
2023-02-08 12:03:28 +0100

    a

--- cabecera.php
+++ cabecera.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="/css/form.css" type="text/css">
+        <link rel="stylesheet" href="/css/form1.css" type="text/css">
         <title>Aridaners - Proyecto PHP</title>
     </head>
 
