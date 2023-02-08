82f57a9e939405b962177a59503f26786787848b
imanol-72 <124252106+imanol-72@users.noreply.github.com>
2023-02-08 12:24:11 +0100

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,6 @@
+ <div id="menu">
+                <ul>
+                     <li><a href="index.php">Inicio</a></li>
+                     <li><a href="index.php?opcion=form">Buscar películas</a></li>
+                </ul>
+</div>
