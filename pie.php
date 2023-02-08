757ced4c3012628de7349276b5471ffe4cd1f9ab
imanol-72 <124252106+imanol-72@users.noreply.github.com>
2023-02-08 12:10:14 +0100

    Update pie.php

--- pie.php
+++ pie.php
@@ -1,39 +1,6 @@
-<div class="footer__container">
-
-<div class="footer__links">
-
-    <div class="footer__link--wrapper">
-
-        <div class="footer__link--items">
-
-            <h2>About Us</h2>
-
-            <a href="/sign__up">How do we work</a> 
-
-            <a href="/">Terms of Service</a>
-                        
-        </div>
-
-
-        <div class="footer__link--items">
-
-            <h2>Contact Us</h2>
-
-            <a href="/">Contact</a> 
-            
-            <a href="/">Support</a>
-
-        </div>
-    </div>
-</div>
-
-
-    <div class="social__media--wrap">
-        <p class="website__rights">© STC 2021. All rights reserved</p>
-    </div>
-</div>          
-<div id="progressbar"></div>
-<div id="scrollPath"></div>
-<script src="/js/scrollbar.js"></script>
-</body>
+        <footer>
+            <p class="website__rights">© ARIDANERS 2023. Todos los derechos reservados</p>
+        </footer>
+        <script src="/js/scrollbar.js"></script>
+    </body>
 </html>
