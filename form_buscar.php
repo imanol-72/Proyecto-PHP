770b4db8c6e1f3e310f9b754bc3cf293fe7ce1f7
BUDIKILLER <124567690+BUDIKILLER@users.noreply.github.com>
2023-02-14 10:41:53 +0100

    Add files via upload

--- form_buscar.php
+++ form_buscar.php
@@ -36,7 +36,7 @@
         <label for="genre">Genero: </label>
         <select name="genre"> 
                 <?php
-                    $categorias=array('Acción','Bélico','Infantil','Drama');
+                    $genero=array('Acción','Bélico','Infantil','Drama');
                     foreach ($genero as $valor)
                     {
                         echo "<option value='$valor' ";
@@ -51,11 +51,11 @@
       <div>
         <h4>Años de estreno</h4>
         <input type="radio" checked="checked" name="option" value="A"  <?php  if (isset($ano)&& ($ano == "a")){echo 'checked = "checked"';} ?>>Todos los años<br>
-        <input type="radio" name="option" value="A" <?php  if (isset($ano)&& in_array("A", $edad)){echo 'checked = "checked"';} ?>>2000 - 2005<br>
-        <input type="radio" name="option" value="B" <?php  if (isset($ano)&& in_array("B", $edad)){echo 'checked = "checked"';} ?>>2006 - 2010<br>
-        <input type="radio" name="option" value="C" <?php  if (isset($ano)&& in_array("C", $edad)){echo 'checked = "checked"';} ?>>2011 - 2015<br>
-        <input type="radio" name="option" value="D" <?php  if (isset($ano)&& in_array("D", $edad)){echo 'checked = "checked"';} ?>>2016 - 2020<br>
-        <input type="radio" name="option" value="E" <?php  if (isset($ano)&& in_array("E", $edad)){echo 'checked = "checked"';} ?>>2021 +<br>
+        <input type="radio" name="option" value="A" <?php  if (isset($ano)&& in_array("A", $ano)){echo 'checked = "checked"';} ?>>2000 - 2005<br>
+        <input type="radio" name="option" value="B" <?php  if (isset($ano)&& in_array("B", $ano)){echo 'checked = "checked"';} ?>>2006 - 2010<br>
+        <input type="radio" name="option" value="C" <?php  if (isset($ano)&& in_array("C", $ano)){echo 'checked = "checked"';} ?>>2011 - 2015<br>
+        <input type="radio" name="option" value="D" <?php  if (isset($ano)&& in_array("D", $ano)){echo 'checked = "checked"';} ?>>2016 - 2020<br>
+        <input type="radio" name="option" value="E" <?php  if (isset($ano)&& in_array("E", $ano)){echo 'checked = "checked"';} ?>>2021 +<br>
       </div>
       <span class="error" ><?php if (isset($errores['ano'])) echo $errores['ano']?></span><br/><br/>
       <!---->  
@@ -69,19 +69,27 @@
        <!---->
       <div>
         <h3>Actores Famosos</h3>
-        <select multiple>
-            <option value="A">Brad Pitt</option>
-            <option value="B">Johnny Depp</option>
-            <option value="C">Bruce Willis</option>
-            <option value="D">Leonardo DiCaprio</option>
-            <option value="E">Angelina Jolie</option>
-            <option value="F">Scarlett Johansson</option>
+        <select name='actores[]' multiple='multiple'>
+            <?php
+                    $actores=array('Brad Pitt','Johnny Depp','Bruce Willis','Leonardo DiCaprio','Angelina Jolie','Scarlett Johansson');
+                    foreach ($actores as $valor)
+                    {
+                        echo "<option value='$valor' ";
+                        if (isset($actores) && in_array($valor, $actores));
+                                {echo "selected = 'selected' ";};
+                        echo ">$valor</option>";
+                    }
+                ?>  
         </select>
       </div>
     
       <div>
         <input type="submit" value="Enviar" />
       </div>
+       <?php
+          if (isset($resultado)) { echo $resultado ;}
+     ?>
     </form>
+     
 </body>
 </html>
