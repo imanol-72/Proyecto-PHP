bf18894ddcd05aa2367605a4ffb5dee11b51b733
BUDIKILLER <124567690+BUDIKILLER@users.noreply.github.com>
2023-02-22 12:52:54 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -7,71 +7,63 @@ require_once 'modelo.php';
 
 if (!isset($_POST['enviar']))
 {
-    include "form_buscar.php";
-    exit();
+     $titulo = trim($_POST['titulo']);
+     $errores = array();
+
+     if (empty($titulo))
+     {
+         $errores['titulo'] = "Introduzca un título";
+     }
+
+     if (isset($_POST['genero']))
+     {
+         $genero = $_POST['genero'];
+     }
+
+     if (isset($_POST['ano']))
+     {
+         $ano = $_POST['ano'];
+     }
+
+     if (!isset($_POST['edad']))
+     {
+         $errores['edad'] = "Seleccione el público objetivo.";
+     }
+     else
+     {
+         $edad = $_POST['edad'];
+     }
+
+     if (!isset($_POST['actores']))
+     {
+         $errores['actores'] = "Seleccione algún actor.";
+     }
+     else
+     {
+         $actores = $_POST['actores'];
+     }
+
+     if (!empty($errores))
+     {
+         include "form_buscar.php";
+         exit();
+     }
+     $titulo = mysqli_real_escape_string($conexion, $titulo);
+     $genero = mysqli_real_escape_string($conexion, $genero);
+     $ano = mysqli_real_escape_string($conexion, $ano);
+     $edad = mysqli_real_escape_string($conexion, $edad);
+     $actores = mysqli_real_escape_string($conexion, $actores);
+
+      $sql = "SELECT  titulo, genero, fechestreno, rangoedad, a.nombre FROM peliculas p JOIN actores a ON p.id=a.codact
+                       WHERE p.titulo LIKE '%$titulo%' and genero=$genero and fechestreno=$ano and rangoedad=$edad and nombre=$actores;";
+
+      $peliculas = obtenerPeliculas($conexion, $sql);
+      include "form_buscar.php";
+      exit();
 }
 
-$titulo = trim($_POST['titulo']);
-$errores = array();
 
-if (empty($titulo))
-{
-    $errores['titulo'] = "Introduzca un título";
-}
-
-if (isset($_POST['genero']))
-{
-    $genero = $_POST['genero'];
-}
-    
-if (isset($_POST['ano']))
-{
-    $ano = $_POST['ano'];
-}
-
-if (!isset($_POST['edad']))
-{
-    $errores['edad'] = "Seleccione el público objetivo.";
-}
-else
-{
-    $edad = $_POST['edad'];
-}
-
-if (!isset($_POST['actores']))
-{
-    $errores['actores'] = "Seleccione algún actor.";
-}
-else
-{
-    $actores = $_POST['actores'];
-}
-
-if (!empty($errores))
-{
-    include "form_buscar.php";
-    exit();
-}
-
-$resultado = "Nombre: ".htmlspecialchars(trim($titulo)).
-            "<br>Género: ".htmlspecialchars(trim($genero)).
-             "<br>Año: ".htmlspecialchars(trim($ano))."<br>Edad: ";
-             $edad = $_POST['edad'];
-            foreach ($edad as $valor)
-            {
-                $resultado .= htmlspecialchars(trim($valor))." ";
-            }
-            $resultado .= "<br>Actores: ";
-            $actores = $_POST['actores'];
-            foreach ($actores as $valor)
-            {
-                $resultado .= htmlspecialchars(trim($valor))." ";
-            }
-
-include "form_buscar.php";
-
-
-   if (isset($_GET['opcion']) && $_GET['opcion'] == 'listar') {
+  if (isset($_GET['opcion']) && $_GET['opcion'] == 'listar') {
           // generar consulta 
           $sql = "select id,titulo,fechestreno,duracion,genero from peliculas;";
           $peliculas = obtenerPeliculas($conexion, $sql);
@@ -79,7 +71,14 @@ include "form_buscar.php";
           exit();
       }
 
-      $sql = "select titulo,duracion,fechestreno from peliculas";
+
+   if (isset($_GET['opcion']) && $_GET['opcion'] == 'buscar') {
+          include "form_buscar.php";
+          exit();
+      }
+
+ 
+      $sql = "SELECT titulo,duracion,fechestreno FROM peliculas";
       $productos = obtenerPeliculas($conexion, $sql);
       include "vista_inicio.php";
 ?>
