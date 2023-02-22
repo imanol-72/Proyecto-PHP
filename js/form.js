4bc240359604888a31e18aa60feb9c30eb1f0ff9
BUDIKILLER <124567690+BUDIKILLER@users.noreply.github.com>
2023-02-22 12:41:54 +0100

    Delete js directory

--- js/form.js
+++
@@ -1,7 +0,0 @@
-var modalBtn = document.querySelector('.modal-btn');
-var modalContent = document.querySelector('.modal-content');
-
-
-modalBtn.addEventListener('click',function(){
-    modalContent.classList.add('content-active')
-});
