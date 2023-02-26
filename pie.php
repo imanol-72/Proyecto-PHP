74fb6754b1d1833cb3dee18695087ad4242a1949
imanol-72 <124252106+imanol-72@users.noreply.github.com>
2023-02-26 11:43:25 +0100

    Delete pie.php

--- pie.php
+++
@@ -1,9 +0,0 @@
-        </div>     <!-- fin de principal-->
-    <div id="pie">
-            <p class="copyright">
-                &copy; <?php echo date("Y"); ?> Aridaners
-            </p>
-     </div>
-    </div><!-- fin de contenido -->
-    </body>
-</html>
