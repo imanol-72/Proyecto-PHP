64610fbddc96d98f56d564a0982d15ea6754b57b
imanol-72 <124252106+imanol-72@users.noreply.github.com>
2023-02-26 11:43:37 +0100

    Delete menu.php

--- menu.php
+++
@@ -1,7 +0,0 @@
- <div id="menu">
-                <ul>
-                    <li><a href="index.php">Inicio</a></li>
-                    <li><a href="form_buscar.php?opcion=buscar"">BUSCAR</a></li>
-                    <li><a href="listar.php?opcion=listar"">LISTAR</a></li>                 
-                </ul>
-</div> <!-- fin de menú  -->   
