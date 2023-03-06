73766523aa8a38bc63fc0aeedb6203d29390f121
dfernangal <124251288+dfernangal@users.noreply.github.com>
2023-03-07 00:36:50 +0100

    Update utils.php
    AÑADIDA FUNCION VALIDAR

--- utils.php
+++ utils.php
@@ -7,10 +7,25 @@
           echo "</pre>";
       }
 
-      function validar($nombre, $apellido)
-      {
-          // completar
-      }
+      function validar($dni, $nombre, $fechnaci)
+     {
+         $errores = array();
+         if (empty($dni))
+         {
+             $errores['dni'] = 'Introduzca dni';
+         }
+         if (empty($nombre))
+         {
+             $errores['nombre'] = 'Introduzca nombre';
+         }
+         
+         if (empty($fechnaci))
+         {
+             $errores['fechnaci'] = 'Introduzca fecha de nacimiento';
+         }
+
+         return $errores;
+     }
 
       function filtrarDato($dato)
       {
