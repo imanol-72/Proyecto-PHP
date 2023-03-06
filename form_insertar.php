409fd2d781fb82425c179f0ad442ff362b896f42
BUDIKILLER <124567690+BUDIKILLER@users.noreply.github.com>
2023-03-06 16:05:58 +0100

    Add files via upload

---
+++ form_insertar.php
@@ -0,0 +1,43 @@
+
+<?php
+     $menu = "menu.php";
+    include "cabecera.php";
+?>
+
+<form id='form' action="index.php" method="post">
+    <div id='datos'>
+    
+     
+        
+    <label>NOMBRE ACTOR</label>
+    <input type="text" name="nombre" value ="<?php if (isset($nombre)) echo filtrarDato($nombre) ?>" />
+    <span class="error"><?php if (isset($errores["nombre"])) echo $errores["nombre"]?></span><br />
+    
+    <label>SALARIO</label> 
+     <?php $salario = array('1500', '2000', '2500' , '3000'); ?>
+      <select name="salario" >
+              <?php
+                foreach ($salario as $sal)
+                {
+                    $e = substr($gen, 0, 1); 
+                    echo "<option value='".$e."'";
+                    echo " >$sal</option>\n";
+                }
+            ?>
+     </select><br />
+    
+     <label>DNI</label>
+    <input type="text" name="dni" value ="<?php if (isset($dni)) echo filtrarDato($dni) ?>" />
+    <span class="error"><?php if (isset($errores["dni"])) echo $errores["dni"]?></span><br />
+    
+    <label>FECHA NACIMIENTO</label>
+    <input type="date" name="fechnaci" value ="<?php if (isset($fech_naci)) echo filtrarDato($fech_naci) ?>" />
+    <span class="error"><?php if (isset($errores["fechnaci"])) echo $errores["fechnaci"]?></span><br />
+    
+    <label>&nbsp;</label>
+    <input type="submit" name="insertar" value="Insertar producto" /><br />
+    </div>
+</form>
+<?php
+    include "pie.php";
+?>
