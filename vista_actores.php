750d497c52201f2a947d1c0f478e89c5d03e1f1e
imanol-72 <124252106+imanol-72@users.noreply.github.com>
2023-03-06 17:58:44 +0100

    Update vista_actores.php

--- vista_actores.php
+++ vista_actores.php
@@ -1,11 +1,11 @@
 <?php 
     $menu = "menu.php"; 
     include "cabecera.php";
-   
+    
     echo "<h2>Actores</h2>";
     foreach ($actores as $actor) 
     {
-        echo "<p>".filtrarDato($actor['nombre'])." -- ".filtrarDato($actor['salario'])." -- ".filtrarDato($actor['fechnaci'])."</p>";
+        echo "<p>".filtrarDato($actor['nombre'])." -- ".filtrarDato($actor['fechnaci'])."</p>";
     }
     include "pie.php";
      
