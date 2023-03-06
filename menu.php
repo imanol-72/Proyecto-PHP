1e09e8cd612c6c6646ee510322272a6ffd3d2393
patupau <124569648+patupau@users.noreply.github.com>
2023-03-06 10:33:23 +0100

    Update menu.php

--- menu.php
+++ menu.php
@@ -2,6 +2,7 @@
                 <ul>
                     <li><a href="index.php">Inicio</a></li>
                     <li><a href="index.php?opcion=buscar">Buscar película</a></li>
-                    <li><a href="index.php?opcion=actores">Actores</a></li>                 
+                    <li><a href="index.php?opcion=actores">Actores</a></li>   
+                    <li><a href="index.php?opcion=borrar">Borrar</a></li>  
                 </ul>
 </div> <!-- fin de menú  -->   
