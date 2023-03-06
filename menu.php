f95e5e2392b088d15e87e8dbf180d617b71d3f11
BUDIKILLER <124567690+BUDIKILLER@users.noreply.github.com>
2023-03-06 16:09:48 +0100

    Update menu.php

--- menu.php
+++ menu.php
@@ -3,6 +3,7 @@
                     <li><a href="index.php">Inicio</a></li>
                     <li><a href="index.php?opcion=buscar">Buscar película</a></li>
                     <li><a href="index.php?opcion=actores">Actores</a></li>   
-                    <li><a href="index.php?opcion=borrar">Borrar</a></li>  
+                    <li><a href="index.php?opcion=borrar">Borrar</a></li>
+                    <li><a href="index.php?opcion=inser">Insertar Actor</a></li>
                 </ul>
 </div> <!-- fin de menú  -->   
