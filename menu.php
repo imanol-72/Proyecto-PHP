49408d2a1a6e650198f7f54faf7801c7c87c685c
dfernangal <124251288+dfernangal@users.noreply.github.com>
2023-03-07 00:40:22 +0100

    Update menu.php

--- menu.php
+++ menu.php
@@ -5,5 +5,6 @@
                     <li><a href="index.php?opcion=actores">Actores</a></li>   
                     <li><a href="index.php?opcion=borrar">Borrar</a></li>
                     <li><a href="index.php?opcion=inser">Insertar Actor</a></li>
+                    <li><a href="index.php?opcion=editar">Editar</a></li>    
                 </ul>
 </div> <!-- fin de menú  -->   
