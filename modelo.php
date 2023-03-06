3dd5c3c8fbc59bfc5c73eafd46db97c6e192995f
dfernangal <124251288+dfernangal@users.noreply.github.com>
2023-03-07 00:39:09 +0100

    Update modelo.php
    AÑADIDAS FUNCIONES PARA MODIFICAR

--- modelo.php
+++ modelo.php
@@ -49,6 +49,50 @@
               exit();
           }
       }
-       
+       function obtenerActoresEdit($conexion, $sql)
+      {
+
+          $resul = mysqli_query($conexion, $sql);
+          if (!$resul) // ha ocurrido un error
+          {
+              $error = "Error en consulta - " . mysqli_error($conexion);
+              include "error.php";
+              exit();
+          }
+//
+          $actorese = []; // guardo en esta array las personas devueltas por la consulta
+          // para mostrar la plantilla de vista_inicio.php
+          while ($fila = mysqli_fetch_array($resul))
+          {
+              $actorese[] = $fila;
+          }
+          return $actorese;
+      }
+      
+      function obtenerActor($conexion, $sql)
+      {
+
+          $resul = mysqli_query($conexion, $sql);
+          if (!$resul) // ha ocurrido un error
+          {
+              $error = "Error en consulta - " . mysqli_error($conexion);
+              include "error.php";
+              exit();
+          }
+          $actore = mysqli_fetch_array($resul);          
+          return $actore;
+      }
+      
+      function modificarActor($conexion, $sql)
+      {
+          $resul = mysqli_query($conexion, $sql);
+          if (!$resul) // ha ocurrido un error
+          {
+              $error = "Error en consulta de edición - " . mysqli_error($conexion);
+              include "error.php";
+              exit();
+          }
+      }
+?>
       
 ?>
