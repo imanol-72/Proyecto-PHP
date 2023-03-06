eb5365a5ff9a1ec395189a3a6ed15a5a4ff2a66f
imanol-72 <124252106+imanol-72@users.noreply.github.com>
2023-03-06 18:04:11 +0100

    Update pie.php

--- pie.php
+++ pie.php
@@ -2,9 +2,12 @@
     
     </div><!-- fin de contenido -->
     <div id="pie">
-            <p class="copyright">
-                &copy; <?php echo date("Y"); ?> Proyecto en PHP y Bases de datos
-            </p>
+        
+        <img src="imagenes/logo.png"/>
+        
+        <p class="copyright">
+                &copy; <?php echo date("Y"); ?> Proyecto en PHP y Bases de datos - ARIDANERS
+        </p>
      </div>
     </body>
 </html>
