adb2eab0497d5397e766ab973abe494a196ee96d
imanol-72 <124252106+imanol-72@users.noreply.github.com>
2023-03-07 18:39:43 +0100

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,10 +1,10 @@
  <div id="menu">
                 <ul>
-                    <li><a href="index.php">Inicio</a></li>
-                    <li><a href="index.php?opcion=buscar">Buscar película</a></li>
-                    <li><a href="index.php?opcion=actores">Actores</a></li>   
-                    <li><a href="index.php?opcion=borrar">Borrar</a></li>
-                    <li><a href="index.php?opcion=inser">Insertar Actor</a></li>
-                    <li><a href="index.php?opcion=editar">Editar</a></li>    
+                    <li><a id="inicio" href="index.php#inicio">Inicio</a></li>
+                    <li><a id="buscar" href="index.php?opcion=buscar#buscar">Buscar película</a></li>
+                    <li><a id="listar" href="index.php?opcion=actores#listar">Actores</a></li>   
+                    <li><a id="borrar" href="index.php?opcion=borrar#borrar">Borrar</a></li>
+                    <li><a id="insertar" href="index.php?opcion=inser#insertar">Insertar Actor</a></li>
+                    <li><a id="editar" href="index.php?opcion=editar#editar">Editar</a></li>    
                 </ul>
 </div> <!-- fin de menú  -->   
